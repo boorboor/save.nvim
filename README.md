3a75c5439384a79844ae26afeb8991f6cb3af1b2
Massoud Boorboor <boorboor@gmail.com>
2024-02-18 01:24:36 +0100

    Refine error handling and optimizations

--- README.md
+++ README.md
@@ -1,8 +1,48 @@
 # save.nvim
-This is small nvim auto save plugin works on `FocusLost` event and has three modes.
-  - No action (default)
-  - Write singe active buffer
-  - Write all buffers
-  - Execute last command
+save.nvim is a Neovim plugin that provides customize able actions triggered on the `FocusLost` event. It allows users to cycle through predefined modes, each associated with a specific action to be executed when Neovim loses focus.
+## Features
+- Cycle through different modes with a single key mapping.
+- Execute actions such as saving all modified buffers or repeating the last command when Neovim loses focus.
+- Easily extendable to add custom actions.
+## Requirements
+- Neovim 0.5.0 or higher
+## Installation
+Manual install
+```bash
+git clone https://github.com/boorboor/save.nvim ~/.local/share/nvim/site/pack/save/start
+```
+Install with [lazy](https://github.com/folke/lazy.nvim)
+```lua
+require("lazy").setup({
+  'boorboor/save.nvim' -- Add to your plugin list
+})
+```
+## Usage
+Setup with `init.vim` file
+```vim
+lua << EOF
+require('save.nvim').setup({
+  change_mode_mapping = '<F4>'  -- Change to your preferred key mapping
+})
+EOF
+```
+Setup with `init.lua` file
+```lua
+require('save.nvim').setup({
+  change_mode_mapping = '<F4>'  -- Change to your preferred key mapping
+})
+```
+### Modes
+The plugin comes with the following modes:
+no action: No action is taken when focus is lost.
+last command: Repeats the last command executed in Neovim.
+write all: Saves all modified buffers.
+write: Saves the current buffer if modified.
 
-Default keymap for mode change is F4.
+Press the change_mode_mapping key (default <F4>) to cycle through the modes. The current mode is displayed in the command line.
+### Customization
+To add custom modes, modify the modes table in the plugin's Lua file. Each mode should have a name and a cmd function that defines the action.
+### Contributing
+Contributions are welcome! If you have a bug report, feature request, or a pull request, please open an issue or submit a PR on GitHub.
+### License
+save.nvim is released under the [MIT License](LICENSE).
--- lua/save/init.lua
+++ lua/save/init.lua
@@ -1,34 +1,74 @@
 local M = {}
 
-M.cmds = {
-  {
-    name = 'no cmd',
-    cmd = function() vim.cmd('echo "auto save off"') end 
-  },
-  {
-    name = 'write',
-    cmd = function() vim.cmd('silent! write |echo "file saved"') end 
-  },
-  {
-    name = 'write all',
-    cmd = function() vim.cmd('silent! write all |echo "files saved"') end 
-  },
-  {
-    name = 'last command',
-    cmd = function() vim.cmd('silent! @: |echo "last command executed"') end
-  },
+local modes = {
+    {
+        name = 'no action',
+    },
+    {
+        name = 'last command',
+        cmd = function()
+            local success, err = pcall(vim.cmd, '@:')
+            if not success then
+                vim.api.nvim_echo({ { "Error: " .. err } }, false, {})
+            end
+        end
+    },
+    {
+        name = 'write all',
+        cmd = function()
+            local success, err = pcall(vim.cmd, 'wall')
+            if not success then
+                vim.api.nvim_echo({ { "Error: " .. err } }, false, {})
+            end
+        end
+    },
+    {
+        name = 'write',
+        cmd = function()
+            if vim.bo.modified then
+                local success, err = pcall(vim.cmd, 'write')
+                if not success then
+                    vim.api.nvim_echo({ { "Error: " .. err } }, false, {})
+                end
+            end
+        end
+    },
 }
 
-M.setup = function()
-  -- auto run command on focus loss, default false
-  vim.api.nvim_create_autocmd('FocusLost', {
-    callback = function() M.cmds[1].cmd() end,
-  })
+local focus_lost_autocmd_id = nil
+-- Default configuration
+local config = {
+    change_mode_mapping = '<F4>',
+}
 
-  vim.keymap.set('n', '<F4>', function()
-    table.insert(M.cmds, 1, table.remove(M.cmds))
-    print('FocusLost mode:', M.cmds[1].name)
-  end)
+local function merge_config(conf)
+    if conf then
+        for key, value in pairs(conf) do
+            config[key] = value
+        end
+    end
+end
+
+M.setup = function(conf)
+    merge_config(conf)
+    vim.api.nvim_create_augroup('SaveFocusLostGroup', { clear = true })
+
+    vim.keymap.set('n', config.change_mode_mapping, function()
+        table.insert(modes, 1, table.remove(modes))
+        if modes[1].name == 'no action' then
+            if focus_lost_autocmd_id then
+                vim.api.nvim_del_autocmd(focus_lost_autocmd_id)
+                focus_lost_autocmd_id = nil
+            end
+        else
+            vim.api.nvim_clear_autocmds({ group = 'SaveFocusLostGroup' })
+            focus_lost_autocmd_id = vim.api.nvim_create_autocmd('FocusLost', {
+                group = 'SaveFocusLostGroup',
+                callback = function() modes[1].cmd() end,
+            })
+        end
+        vim.api.nvim_echo({ { 'FocusLost mode: ' .. modes[1].name } }, false, {})
+    end)
 end
 
 return M
